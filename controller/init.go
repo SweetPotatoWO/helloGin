4d0ec75b07f0224f9d826cc2f0d2a10e1fb36400
xupeiqin <768049839@qq.com>
2020-01-19 14:20:47 +0800

    修改版本

--- controller/init.go
+++ controller/init.go
@@ -1,8 +1,9 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
 	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 type ControllerInter interface {
@@ -18,8 +19,8 @@ type InitController struct {
 
 func (cInit *InitController) Run(c *gin.Engine, controllerFnCode string) {
 	//1 先获取到请求所传递过来的参数
-	param,err := cInit.getParams(c)
-	if err != nil  {
+	param, err := cInit.getParams(c)
+	if err != nil {
 		log.Fatalf("获取到参数错误", err)
 	}
 
@@ -29,39 +30,38 @@ func (cInit *InitController) Run(c *gin.Engine, controllerFnCode string) {
 		log.Fatalf("initBefore执行失败", err)
 	}
 	//3 对控制器的名字和方法名进行解析
-	controllerAndFnName,err := cInit.splitControllerAndFnName(controllerFnCode)
-	if err!= nil {
+	controllerAndFnName, err := cInit.splitControllerAndFnName(controllerFnCode)
+	if err != nil {
 		log.Fatalf("找不到控制器", err)
 	}
 	//实例化对应的控制器
 	var ControllerName = controllerAndFnName["controller"]
 	var RuqFnName = controllerAndFnName["fnname"]
-	 
 
 	//
 	cInit.initAfterFn(param)
 }
 
-
 //返回对应的map 和 错误 也就是获取到控制器的名字和对应调用的方法名字
 //格式 为 User/addUser
-func (cInit *InitController) splitControllerAndFnName(controllerFnCode) (map[string]string,error) {
-	return nil,nil
+func (cInit *InitController) splitControllerAndFnName(controllerFnCode string) (map[string]string, error) {
+
+	return nil, nil
 }
 
 //在调用某个子类之前会提前调用 可在这里写一些通用的逻辑
 //适用于全部的基础类 必须返回true 只有返回true的情况才会继续运行
-func (cInit *InitController) initBeforeFn() bool {
+func (cInit *InitController) initBeforeFn(param interface{}) bool {
 	return true
 }
 
 //在调用某个子类之前会提前调用 可在这里写一些通用的逻辑
 //适用于全部的基础类 必须返回true 只有返回true的情况才会继续运行
-func (cInit *InitController) initAfterFn() bool {
+func (cInit *InitController) initAfterFn(param interface{}) bool {
 	return true
 }
 
 //获取到每次传递过来的参数
-func (cInit *InitController) getParams(c *gin.Engine) (interface{},error){
-	return {},nil
+func (cInit *InitController) getParams(c *gin.Engine) (interface{}, error) {
+	return nil, nil
 }
